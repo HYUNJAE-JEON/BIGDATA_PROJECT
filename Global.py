200071d3227d02b5079a4e0e94e9c820d441d18d
HYUNJAE-JEON <ysysyshy@gamil.com>
2020-12-03 15:01:11 +0900

    2020_BEMS_FINAL_PROJECT

---
+++ Global.py
@@ -0,0 +1,8 @@
+os.putenv("NLS_LANG", "KOREAN_KOREA.KO16KSC5601")
+
+# DB 접속용 변수
+MARIA_DB_USER = 'root'
+MARIA_DB_PASS = 'password'
+MARIA_DB_IP = '127.0.0.1'
+MARIA_DB_PORT = '3306'
+MARIA_DB_NAME = 'EMS_DCS'
